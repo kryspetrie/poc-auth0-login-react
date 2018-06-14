5a8c2f211ab9db0588060f96e622f3b9e53c62f8
Krys Petrie <krys.petrie@trinetx.com>
2018-06-14 17:11:09 -0400

    Updating clientID to use new trinetx auth0 account.

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
-  domain: 'kryspetrie.auth0.com',
-  clientId: '3BXTx9n34wyBC47pQxpp5clq2g8LCdhU',
+  domain: 'trinetx.auth0.com',
+  clientId: '5PM2s3DRBZ7KqPVaPu5lJ9zDp6ppvXrt',
   baseUrl: 'http://localhost:3000',
   redirectRoute: '/callback',
   logoutRoute: '/'
